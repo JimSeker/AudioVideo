055ae327ed101aef83bb063388942834f99414e0
JimSeker <seker@uwyo.edu>
2015-05-20 16:32:28 -0600

    changed up to fragments

--- PicCapture2/app/src/main/java/edu/cs4730/piccapture2/MainActivity.java
+++ PicCapture2/app/src/main/java/edu/cs4730/piccapture2/MainActivity.java
@@ -1,204 +1,46 @@
 package edu.cs4730.piccapture2;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.hardware.Camera.PictureCallback;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
 
 /*
  * This example based on android's example on there pages
  * http://developer.android.com/guide/topics/media/camera.html
- * 
- * The user presses a button in order to capture the picture.
+ * but it has been changed a whole lot, plus bug fixes from android... dumb ones too.
  *
- * the error in the code has been fixed.  Now change this example to use fragments.
+ * The user presses a button in order to capture the picture and the surfaceview is a seperate class.
+ *
+ * most of the code is in the MainFragment and/or the surfaceView.  The code here is for the onPause
+ * and onResume events, so we don't hold the camera while the app is paused.
  *
  */
 public class MainActivity extends AppCompatActivity {
 
-    private Camera mCamera;
-    private CameraPreview mPreview;
-    static int MEDIA_TYPE_IMAGE = 1;
-    static int MEDIA_TYPE_VIDEO = 2;
-    String TAG = "MainActivity";
-    FrameLayout preview;
+    MainFragment mf;
 
+    String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
-
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
-
-        // Add a listener to the Capture button
-        Button captureButton = (Button) findViewById(R.id.button_capture);
-        captureButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // get an image from the camera
-                        mCamera.takePicture(null, null, mPicture);
-                    }
-                }
-        );
-
+        if (mf == null) {
+            mf = new MainFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, mf).commit();
+        }
     }
 
 
     @Override
     public void onPause() {
-        super.onPause();
-
-        releaseCamera();              // release the camera immediately on pause event
+        super.onPause();     //call the super first, then our stuff.
+        mf.releaseCamera();   // release the camera immediately on pause event
     }
 
     @Override
     public void onResume() {
         super.onResume();  // Always call the superclass method first
-
-        // Get the Camera instance as the activity achieves full user focus
-        if (mCamera == null && preview != null) {
-            mCamera = getCameraInstance(); // Local method to handle camera init
-            //mPreview = null;  //close down the old one?  maybe, should end the surface, so we can restart it.
-            mPreview = new CameraPreview(this, mCamera);
-            preview.addView(mPreview);
-        }
-    }
-    private void releaseCamera() {
-        if (mCamera != null) {
-            mCamera.release();        // release the camera for other applications
-            mCamera = null;
-            preview.removeView(mPreview);  //remove the surfaceView or it will try and use the camera after it has been released.
-            mPreview = null;
-        }
-    }
-
-    /**
-     * A safe way to get an instance of the Camera object.
-     */
-    public static Camera getCameraInstance() {
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        } catch (Exception e) {
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
-    }
-
-    /**
-     * Check if this device has a camera
-     */
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
-    }
-
-    private PictureCallback mPicture = new PictureCallback() {
-
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-
-            //another way to create and write out a file, instead of what was done in picCapture
-            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-            if (pictureFile == null) {
-                Log.d(TAG, "Error creating media file, check storage permissions: ");
-                return;
-            }
-            Log.d(TAG, "File created as:" + pictureFile);
-            //write out the file.
-            try {
-                FileOutputStream fos = new FileOutputStream(pictureFile);
-                fos.write(data);
-                fos.close();
-            } catch (FileNotFoundException e) {
-                Log.d(TAG, "File not found: " + e.getMessage());
-            } catch (IOException e) {
-                Log.d(TAG, "Error accessing file: " + e.getMessage());
-            }
-
-            //new tell the system that the file exists , so it will show up in gallery/photos/whatever
-            ContentValues values = new ContentValues();
-            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
-            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-            values.put(MediaStore.MediaColumns.DATA, pictureFile.toString());
-            getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-
-            camera.startPreview();  //start backup the preview, once we have taken the picture.
-        }
-    };
-
-    /**
-     * Create a file Uri for saving an image or video
-     */
-    private static Uri getOutputMediaFileUri(int type) {
-        return Uri.fromFile(getOutputMediaFile(type));
-    }
-
-    /**
-     * Create a File for saving an image or video
-     */
-    private static File getOutputMediaFile(int type) {
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-
-        //creates a directory in pictures.
-        //File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyCameraApp");
-
-        File mediaStorageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
-                Log.d("MyCameraApp", "failed to create directory");
-                return null;
-            }
-        }
-
-        // Create a media file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File mediaFile;
-        if (type == MEDIA_TYPE_IMAGE) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "IMG_" + timeStamp + ".jpg");
-        } else if (type == MEDIA_TYPE_VIDEO) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "VID_" + timeStamp + ".mp4");
-        } else {
-            return null;
-        }
-
-        return mediaFile;
+        mf.reinitCamera(); //reinitialize the camera if coming from an onPause event.
     }
 
 }
--- PicCapture2/app/src/main/java/edu/cs4730/piccapture2/MainFragment.java
+++ PicCapture2/app/src/main/java/edu/cs4730/piccapture2/MainFragment.java
@@ -1,18 +1,49 @@
 package edu.cs4730.piccapture2;
 
-
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
-/**
- * A simple {@link Fragment} subclass.
+/*
+ * So this example is similar to picCapture1 but it is split up better.
+ * The surfaceview is separated, so it's easier to see what pieces belong to what.
+ * Also this code will pause and release the camera, so others can use it.
+ * And it shows how to write a file and then tell the system it exists, so photos/gallery/whatever
+ * can file the new pictures.
+ *
+ * lastly, yes. Camera is deprecated in 21+, but for older devices, you will still need it.
+ * And some point, I hope to get another fragment that deals with the Camera2 version.  then have
+ * the mainActivity use the correct one based on the version of the device.
+ *
  */
-public class MainFragment extends Fragment {
 
+
+public class MainFragment extends Fragment {
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    static int MEDIA_TYPE_IMAGE = 1;
+    static int MEDIA_TYPE_VIDEO = 2;
+    String TAG = "MainFragment";
+    FrameLayout preview;
 
     public MainFragment() {
         // Required empty public constructor
@@ -23,7 +54,149 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View myView = inflater.inflate(R.layout.fragment_main, container, false);
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(getActivity().getApplicationContext(), mCamera);
+        preview = (FrameLayout) myView.findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
+        // Add a listener to the Capture button
+        Button captureButton = (Button) myView.findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        mCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );
+        return myView;
+    }
+    public void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+            preview.removeView(mPreview);  //remove the surfaceView or it will try and use the camera after it has been released.
+            mPreview = null;
+        }
+    }
+
+    public void reinitCamera() {
+        // Get the Camera instance as the activity achieves full user focus
+        if (mCamera == null && preview != null) {
+            mCamera = getCameraInstance(); // Local method to handle camera init
+            //mPreview = null;  //close down the old one?  maybe, should end the surface, so we can restart it.
+            mPreview = new CameraPreview(getActivity().getApplicationContext(), mCamera);
+            preview.addView(mPreview);
+        }
+    }
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    /**
+     * Check if this device has a camera
+     */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            //another way to create and write out a file, instead of what was done in picCapture
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null) {
+                Log.d(TAG, "Error creating media file, check storage permissions: ");
+                return;
+            }
+            Log.d(TAG, "File created as:" + pictureFile);
+            //write out the file.
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+
+            //new tell the system that the file exists , so it will show up in gallery/photos/whatever
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+            values.put(MediaStore.MediaColumns.DATA, pictureFile.toString());
+            getActivity().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+            camera.startPreview();  //start backup the preview, once we have taken the picture.
+        }
+    };
+
+    /**
+     * Create a file Uri for saving an image or video
+     */
+    private static Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /**
+     * Create a File for saving an image or video
+     */
+    private static File getOutputMediaFile(int type) {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        //creates a directory in pictures.
+        //File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyCameraApp");
+
+        File mediaStorageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_" + timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
     }
 
 
