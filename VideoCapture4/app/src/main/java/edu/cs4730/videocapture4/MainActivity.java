aa8974bfc5c7c76e6ccf3b88793154540b813ead
JimSeker <seker@uwyo.edu>
2016-02-11 12:53:53 -0700

    added new example and fixed a minor mistake in preview.

--- VideoCapture4/app/src/main/java/edu/cs4730/videocapture4/MainActivity.java
+++ VideoCapture4/app/src/main/java/edu/cs4730/videocapture4/MainActivity.java
@@ -1,20 +1,18 @@
-package edu.cs4730.camerapreview;
+package edu.cs4730.videocapture4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener  {
 
     public static final int REQUEST_PERM_ACCESS = 1;
     String TAG = "MainActivity";
     MainFragment myFrag;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         if (savedInstanceState == null) {
             myFrag = new MainFragment();
             getSupportFragmentManager().beginTransaction()
@@ -24,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnFr
 
     @Override
     public void onFragmentInteraction(int which) {
-       if (which == 1) {
+        if (which == 1) {
 
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, new Cam2Fragment()).addToBackStack(null).commit();
