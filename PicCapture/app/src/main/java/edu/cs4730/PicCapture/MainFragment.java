432727d59a3506d9e478b7920773c2644241eb3c
JimSeker <seker@uwyo.edu>
2017-01-17 11:40:06 -0700

    updated API 25.

--- PicCapture/app/src/main/java/edu/cs4730/PicCapture/MainFragment.java
+++ PicCapture/app/src/main/java/edu/cs4730/PicCapture/MainFragment.java
@@ -3,7 +3,6 @@ package edu.cs4730.PicCapture;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -13,10 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.concurrent.Callable;
-
-import edu.cs4730.PicCapture.R;
 
 /**
  * A simple {@link Fragment} subclass.
--- PicCapture3/app/src/main/java/edu/cs4730/piccapture3/MainActivity.java
+++ PicCapture3/app/src/main/java/edu/cs4730/piccapture3/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /*
  * A simple example of using an intent to take a picture and display it on the screen
@@ -32,7 +33,13 @@ public class MainActivity extends AppCompatActivity {
         //the picture is stored in the intent in the data key.
         //get the picture and show it in an the imagview.
         //Note the picture is not stored on the filesystem, so this is the only "copy" of the picture.
-        Bitmap bp = (Bitmap) data.getExtras().get("data");
-        mf.setPic(bp);
+        Bundle extras = data.getExtras();
+        if (extras != null) {
+            //if you know for a fact there will be a bundle, you can use  data.getExtras().get("Data");  but we don't know.
+            Bitmap bp = (Bitmap) extras.get("data");
+            mf.setPic(bp);
+        } else {
+            Toast.makeText(this, "No picture was returned", Toast.LENGTH_SHORT).show();
+        }
     }
 }
