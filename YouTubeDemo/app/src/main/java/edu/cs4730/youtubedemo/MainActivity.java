2f0996aa81ddee2fd6357e17cdcf77cf1cf04174
Jim Ward <seker@uwyo.edu>
2021-01-18 11:01:35 -0700

    updated, but fails in API 30.  so still at API 29.

--- YouTubeDemo/app/src/main/java/edu/cs4730/youtubedemo/MainActivity.java
+++ YouTubeDemo/app/src/main/java/edu/cs4730/youtubedemo/MainActivity.java
@@ -1,22 +1,27 @@
 package edu.cs4730.youtubedemo;
 
 import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 
 /**
  * Youtube demo
- *
+ * <p>
  * Helpful references:
  * https://developers.google.com/youtube/android/player/
- *   as a note, this is currently use the 1.2.2 jar files which was the most recent as of Oct 30, 2018
- *
+ * as a note, this is currently use the 1.2.2 jar files which was the most recent as of Oct 30, 2018
+ * <p>
  * a nice tutorial of how to implement it.
  * http://www.sitepoint.com/using-the-youtube-api-to-embed-video-in-an-android-app/
- *
- *
+ * <p>
+ * <p>
  * To get this example to work see the DeveloperKey.java file to change the API key.
+ *
+ *
+ * This fails in API 30.  
  */
 
 
@@ -46,17 +51,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Second method,  An advanced example with more control and feedback.
-        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            //dies.
-            //   getSupportFragmentManager().beginTransaction().add(android.R.id.content, new MainFragment()).commit();
 
-        }
-    });
-
-}
+    }
 
 
 }
