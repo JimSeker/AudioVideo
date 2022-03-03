7001a077e6a99766ac4dd0e5111b27baa4f871c5
Jim Ward <seker@uwyo.edu>
2022-03-03 10:02:28 -0700

    lots of clean up.  removed unneeded fragment.

--- AudioPlay/app/src/main/java/edu/cs4730/AudioPlay/MainActivity.java
+++ AudioPlay/app/src/main/java/edu/cs4730/AudioPlay/MainActivity.java
@@ -1,45 +1,155 @@
 package edu.cs4730.AudioPlay;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 /**
  * A simple app to demo how to play/pause/restart playing an audio
- * file from different places.  See the fragment for actually code.
+ * file from different places.
  */
 
 public class MainActivity extends AppCompatActivity {
 
-    MainFragment myFrag;
+    static final String AUDIO_PATH = "http://www.cs.uwyo.edu/~seker/courses/4730/example/MEEPMEEP.WAV";
+    //"http://java.sun.com/products/java-media/mma/media/test-wav.wav";  //doesn't exist anymore.
+    //"/sdcard/file.mp3"
+    MediaPlayer mediaPlayer;
+    int playbackPosition = 0;
+    boolean localfile = false;
+
+
+    Button btnStartl, btnStart, btnPause, btnRestart;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            myFrag = new MainFragment();
+        btnStartl = findViewById(R.id.btnPlayLocal);
+        btnStartl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playAudioResource();
+            }
+        });
+        // play button, using remote file.
+        btnStart = findViewById(R.id.btnPlay);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playAudio(AUDIO_PATH);
+            }
+        });
+        //pause the audio.
+        btnPause = findViewById(R.id.btnPause);
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pauseAudio();
+            }
+        });
+        //restart the audio.
+        btnRestart = findViewById(R.id.btnRestart);
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                restartAudio();
+            }
+        });
+    }
 
-            getSupportFragmentManager().beginTransaction()
-                .add(R.id.container, myFrag).commit();
+    /**
+     * simple code to pause the playback and store the current position.
+     */
+    void pauseAudio() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            playbackPosition = mediaPlayer.getCurrentPosition();
+            mediaPlayer.pause();
         }
     }
 
     /**
+     * Load a file from the res/raw directory and play it.
+     */
+    void playAudioResource() {
+        if (mediaPlayer == null || !localfile) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.hmscream);
+            localfile = true;
+        } else { //play it again sam
+            mediaPlayer.seekTo(0);
+        }
+        if (mediaPlayer.isPlaying()) {  //duh don't start it again.
+            Toast.makeText(this, "I'm playing already", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //finally play!
+        mediaPlayer.start();
+    }
+
+    /**
+     * load a file from some url (including the internet) and play it.
+     */
+    void playAudio(String url) {
+        if (mediaPlayer == null || localfile) {//first time or not the right file.
+            localfile = false;
+            mediaPlayer = new MediaPlayer();
+            try {
+                mediaPlayer.setDataSource(url);
+                mediaPlayer.prepare();
+            } catch (Exception e) {
+                Toast.makeText(this, "Exception, not playing",
+                    Toast.LENGTH_SHORT).show();
+                System.out.println("Player exception is " + e.getMessage());
+                return;
+            }
+        } else if (mediaPlayer.isPlaying()) {  //duh don't start it again.
+            Toast.makeText(this, "I'm playing already", Toast.LENGTH_SHORT).show();
+            return;
+        } else {  //play it at least one, reset and play again.
+            mediaPlayer.seekTo(0);
+        }
+        mediaPlayer.start();
+
+    }
+
+    /**
+     * should be called after the pauseAudio(), but setup has position set to zero so should work
+     * anyway.
+     */
+    void restartAudio() {
+        if (mediaPlayer != null && !mediaPlayer.isPlaying()) {
+            mediaPlayer.seekTo(playbackPosition);
+            mediaPlayer.start();
+        }
+    }
+
+    /**
+     * make sure the player has been released.
+     */
+    void KillMediaPlayer() {
+        if (mediaPlayer != null)
+            mediaPlayer.release();
+        mediaPlayer = null;
+    }
+
+    /**
      * Make sure we clean up and release the media player in on pause (and onDestroy too)
-     * (non-Javadoc)
-     *
-     * @see FragmentActivity#onPause()
      */
     @Override
     protected void onPause() {
-        myFrag.KillMediaPlayer();
+        KillMediaPlayer();
         super.onPause();
     }
 
     @Override
     protected void onDestroy() {
-        myFrag.KillMediaPlayer();
+        KillMediaPlayer();
         super.onDestroy();
     }
 
