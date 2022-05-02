c2ba78747f4005be0c2759627f044f08de7b95a4
Jim Ward <seker@uwyo.edu>
2022-05-02 10:57:36 -0600

    updated to permissions and file names to add more flexibility.

--- AudioRecordTest/app/src/main/java/edu/cs4730/AudioRecord/MainActivity.java
+++ AudioRecordTest/app/src/main/java/edu/cs4730/AudioRecord/MainActivity.java
@@ -1,21 +1,19 @@
 package edu.cs4730.AudioRecord;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Toast;
 
 /**
  * Almost nothing interesting here.  It deals with the permission request for the file system and audio record access.
  */
 
 public class MainActivity extends AppCompatActivity {
-    String TAG = "MainActivity";
+    public static final String TAG = "MainActivity";
     private MainFragment myFrag;
-    public static final int REQUEST_PERM_ACCESS = 1;
+    static int MEDIA_TYPE_IMAGE = 1;
+    static int MEDIA_TYPE_VIDEO = 2;
+    static int MEDIA_TYPE_AUDIO = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,44 +26,5 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        Log.v(TAG, "onRequest result called.");
-        boolean file = false, mic = false;
-
-        switch (requestCode) {
-            case REQUEST_PERM_ACCESS:
-                //received result for GPS access
-                Log.v(TAG, "Received response for permissions request.");
-                for (int i = 0; i < grantResults.length; i++) {
-                    if ((permissions[i].compareTo(Manifest.permission.WRITE_EXTERNAL_STORAGE) == 0) &&
-                        (grantResults[i] == PackageManager.PERMISSION_GRANTED))
-                        file = true;
-                    else if ((permissions[i].compareTo(Manifest.permission.RECORD_AUDIO) == 0) &&
-                        (grantResults[i] == PackageManager.PERMISSION_GRANTED))
-                        mic = true;
-                }
-                if (mic && file) {
-                    // permission was granted
-                    Log.v(TAG, "Both permissions has now been granted. Starting Demo.");
-                    myFrag.startRecording();
-                } else {
-                    // permission denied,    Disable this feature or close the app.
-                    Log.v(TAG, "both permissions were NOT granted.");
-                    Toast.makeText(this, "File and Mic access NOT granted", Toast.LENGTH_SHORT).show();
-                    //clean up interface.
-                    myFrag.recording = true;
-                    myFrag.btn_record.setText("Start recording");
-                }
-
-                return;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
 
 }
--- AudioRecordTest/app/src/main/java/edu/cs4730/AudioRecord/MainFragment.java
+++ AudioRecordTest/app/src/main/java/edu/cs4730/AudioRecord/MainFragment.java
@@ -2,26 +2,38 @@ package edu.cs4730.AudioRecord;
 
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import androidx.core.app.ActivityCompat;
-import androidx.core.os.EnvironmentCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
-import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * Shows how to setup and record audio.
@@ -32,34 +44,55 @@ import java.io.IOException;
 public class MainFragment extends Fragment {
 
     String TAG = "MainFragment";
-    private static String mFileName = null;
+    private String[] REQUIRED_PERMISSIONS;
+    ActivityResultLauncher<String[]> rpl;
     private MediaRecorder mRecorder = null;
     private MediaPlayer mPlayer = null;
-
+    Uri mFileUri;
+    TextView logger;
     Boolean recording, playing;
     Button btn_play, btn_record;
 
     public MainFragment() {
         recording = true;
         playing = true;
+        //Use this to check permissions.
+        rpl = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
+            new ActivityResultCallback<Map<String, Boolean>>() {
+                @Override
+                public void onActivityResult(Map<String, Boolean> isGranted) {
+                    boolean granted = true;
+                    for (Map.Entry<String, Boolean> x : isGranted.entrySet()) {
+                        logthis(x.getKey() + " is " + x.getValue());
+                        if (!x.getValue()) granted = false;
+                    }
+                    if (granted) startRecording();
+                }
+            }
+        );
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.fragment_main, container, false);
-
-        //moved everything to the external download directory for simplification.  really need to figure out to start in music/video directory.
-        //note this only works, because it a media file, I think.  can't use generic files.  Can't change to the .MUSIC either.
-        mFileName = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
-       // mFileName = getActivity().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();  //local app download directory.
-        mFileName += File.separator + "audiorecordtest.3gp";
-        Log.wtf(TAG, mFileName);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {  //For API 29+ (q), for 26 to 28.
+            REQUIRED_PERMISSIONS = new String[]{Manifest.permission.RECORD_AUDIO};
+        } else {
+            REQUIRED_PERMISSIONS = new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        }
+        logger = myView.findViewById(R.id.logger);
         btn_play = myView.findViewById(R.id.btn_play);
         btn_play.setOnClickListener(mlistener);
         btn_record = myView.findViewById(R.id.btn_record);
         btn_record.setOnClickListener(mlistener);
         return myView;
+    }
+
+    //A simple method to append data to the logger textview.
+    public void logthis(String msg) {
+        logger.append(msg + "\n");
+        Log.d(TAG, msg);
     }
 
     View.OnClickListener mlistener = new View.OnClickListener() {
@@ -96,11 +129,12 @@ public class MainFragment extends Fragment {
     private void startPlaying() {
         mPlayer = new MediaPlayer();
         try {
-            mPlayer.setDataSource(mFileName);
+            logthis("start playing.");
+            mPlayer.setDataSource(requireContext(), mFileUri);
             mPlayer.prepare();
             mPlayer.start();
         } catch (IOException e) {
-            Log.e(TAG, "prepare() failed");
+            logthis("prepare() failed");
         }
     }
 
@@ -109,6 +143,7 @@ public class MainFragment extends Fragment {
      * if you want to continue it later, don't release, so audio play example.
      */
     private void stopPlaying() {
+        logthis("stop playing.");
         mPlayer.stop();  //maybe not be needed, since just releasing anyway.
         mPlayer.release();
         mPlayer = null;
@@ -121,23 +156,45 @@ public class MainFragment extends Fragment {
      */
 
     public void startRecording() {
-        if ((ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) ||
-            (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)) {
-            //I'm on not explaining why, just asking for permission.
-            Log.v(TAG, "asking for permissions");
-            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO},
-                MainActivity.REQUEST_PERM_ACCESS);
-
+        if (!allPermissionsGranted()) {
+            rpl.launch(REQUIRED_PERMISSIONS);
         } else {
+            logthis("Start recording");
             mRecorder = new MediaRecorder();
-            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            mRecorder.setOutputFile(mFileName);
-            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            //Setting this to record the NOT local, ie the sd card directory for audio.
+            //if set to true.  no write permissions are needed in any api version.
+            mFileUri = getOutputMediaFile(MainActivity.MEDIA_TYPE_AUDIO, true);
+
+            //this code is just to print the real filename, instead of the contentresolver number name.
+            //logthis("file name is " + mFileUri.toString());
+            String[] filePathColumn = {MediaStore.Audio.Media.DATA};
+            Cursor cursor = requireActivity().getContentResolver().query(mFileUri, filePathColumn, null, null, null);
+            String file;
+            if (cursor != null) {  //sdcard
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                file = cursor.getString(columnIndex);
+                cursor.close();
+            } else { //local
+                file = mFileUri.toString();
+            }
+            logthis("File name is " + file);
+
+            //now open the file and set to write.
+            try {
+                ParcelFileDescriptor pfd = requireActivity().getContentResolver().openFileDescriptor(mFileUri, "w");
+                mRecorder.setOutputFile(pfd.getFileDescriptor());
+                mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            } catch (FileNotFoundException e) {
+                logthis("openFileDescriptor failed");
+            }
+
             try {
                 mRecorder.prepare();
             } catch (IOException e) {
-                Log.e(TAG, "prepare() failed");
+                logthis("prepare() failed");
             }
             mRecorder.start();
         }
@@ -148,6 +205,7 @@ public class MainFragment extends Fragment {
      * Note if you want to stop and then continue again, you would need to stop, but not release (and then only start() again).
      */
     private void stopRecording() {
+        logthis("Stop recording");
         mRecorder.stop();
         mRecorder.release();
         mRecorder = null;
@@ -156,12 +214,7 @@ public class MainFragment extends Fragment {
 
     /**
      * clean up and make sure I have released the player and recorder.
-     *
-     * (non-Javadoc)
-     *
-
      */
-
     @Override
     public void onPause() {
         super.onPause();
@@ -174,4 +227,76 @@ public class MainFragment extends Fragment {
             mPlayer = null;
         }
     }
+
+
+    /**
+     * Create a File for saving an image or video
+     */
+    public Uri getOutputMediaFile(int type, boolean local) {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        ContentValues values = new ContentValues();
+        File mediaFile;
+        File storageDir;
+        Uri returnUri = null;
+
+        if (type == MainActivity.MEDIA_TYPE_IMAGE) {
+
+            if (local) {
+                storageDir = requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                if (!storageDir.exists()) {
+                    storageDir.mkdirs();
+                }
+                mediaFile = new File(storageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+                returnUri = Uri.fromFile(mediaFile);
+
+            } else { //onto the sdcard
+                //values.put(MediaStore.Images.Media.TITLE, "IMG_" + timeStamp + ".jpg");  //not needed?
+                values.put(MediaStore.Images.Media.DISPLAY_NAME, "IMG_" + timeStamp + ".jpg");  //file name.
+                values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg");
+                returnUri = requireActivity().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            }
+        } else if (type == MainActivity.MEDIA_TYPE_VIDEO) {
+            if (local) {
+                storageDir = requireActivity().getExternalFilesDir(Environment.DIRECTORY_MOVIES);
+                if (!storageDir.exists()) {
+                    storageDir.mkdirs();
+                }
+                mediaFile = new File(storageDir.getPath() + File.separator + "VID_" + timeStamp + ".mp4");
+                returnUri = Uri.fromFile(mediaFile);
+            } else {
+                //values.put(MediaStore.Images.Media.TITLE, "VID_" + timeStamp + ".mp4");  //not needed?
+                values.put(MediaStore.Video.Media.DISPLAY_NAME, "VID_" + timeStamp + ".mp4");  //file name.
+                values.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4");
+                returnUri = requireActivity().getContentResolver().insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
+            }
+        } else if (type == MainActivity.MEDIA_TYPE_AUDIO) {
+            if (local) {
+                storageDir = requireActivity().getExternalFilesDir(Environment.DIRECTORY_MUSIC);
+                if (!storageDir.exists()) {
+                    storageDir.mkdirs();
+                }
+                mediaFile = new File(storageDir.getPath() + File.separator + "AUD_" + timeStamp + ".mp4");
+                returnUri = Uri.fromFile(mediaFile);
+            } else {
+                values.put(MediaStore.Audio.Media.DISPLAY_NAME, "AUD_" + timeStamp + ".mp3");  //file name.
+                values.put(MediaStore.Audio.Media.MIME_TYPE, "audio/mp3");
+                returnUri = requireActivity().getContentResolver().insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, values);
+            }
+        }
+        return returnUri;
+    }
+
+    /**
+     * This a helper method to check for the permissions.
+     */
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
