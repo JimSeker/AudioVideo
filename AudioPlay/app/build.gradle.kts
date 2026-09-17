3a1817bea75a44a88aae60081b2168171a9c1623
Jim Ward <JimSeker@users.noreply.github.com>
2026-09-17 14:43:09 -0600

    updated gradle and groovy to kts.

---
+++ AudioPlay/app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "edu.cs4730.AudioPlay"
+    compileSdk = 37
+    defaultConfig {
+        applicationId = "edu.cs4730.AudioPlay"
+        minSdk = 32
+        targetSdk = 37
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.txt")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
+}
